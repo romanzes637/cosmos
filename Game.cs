1fea76ac35091e70dad82f0a006d36cc68d80fe6
Romanzes <romanbutov637@gmail.com>
2017-06-22 22:25:03 +0300

    Unity PopulationViewModel and Game scripts added

---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+	public PopulationViewModel population;
+	public int nPopulations;
+	public int range;
+	public Dictionary<int, PopulationViewModel> populations;
+	public Dictionary<int, Cosmos.Resource> resources;
+	public float dt;
+	public float globalPopulation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		populations = new Dictionary<int, PopulationViewModel> ();
+		resources = new Dictionary<int, Cosmos.Resource> ();
+		for (int i = 0; i < nPopulations; i++) {
+			populations.Add (i, Instantiate (population,
+				transform.position + new Vector3 (
+					Cosmos.Global.random.Next (-range, range), 
+					Cosmos.Global.random.Next (-range, range), 
+					Cosmos.Global.random.Next (-range, range)),
+				Quaternion.identity, transform));
+			resources.Add (i, new Cosmos.Resource (10000));
+			populations [i].AddResource (resources [i]);
+			populations [i].SetWorkers (0, populations [i].value);
+		}
+
+		for (int i = 0; i < nPopulations; i++) {
+			for (int j = 0; j < nPopulations; j++) {
+				if (j != i) {
+					populations [i].AddPopulation (j, populations [j].population);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (KeyValuePair<int, PopulationViewModel> entry in populations) {
+//			Debug.Log (populations.Count);
+			entry.Value.UpdatePopulation (dt);
+		}
+		foreach (KeyValuePair<int, PopulationViewModel> entry in populations) {
+			entry.Value.Migrate (dt);
+		}
+		List<int> removeKeys = new List<int> ();
+		globalPopulation = 0;
+		foreach (KeyValuePair<int, PopulationViewModel> entry in populations) {
+			bool ext = entry.Value.CheckExtinction ();
+			globalPopulation += entry.Value.value;
+			entry.Value.UpdatePlot (dt);
+			if (ext) {
+				removeKeys.Add (entry.Key);
+			}
+		}
+		foreach (int key in removeKeys) {
+			foreach (KeyValuePair<int, PopulationViewModel> entry in populations) {
+				entry.Value.RemovePopulation (key);
+			}
+			Destroy (populations [key].gameObject);
+			populations.Remove (key);
+		}
+	}
+}
---
+++ PopulationViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationViewModel : MonoBehaviour
+{
+	public Cosmos.GaussProdConsResMigPopulation population { get; private set; }
+
+	public float initialValue;
+	public float a;
+	public float pMean;
+	public float pDev;
+	public float cMean;
+	public float cDev;
+	public float cRate;
+	public float mA;
+	public float value;
+	public float nEmigrants;
+	public List<float> workers;
+	public List<Cosmos.Resource> resources;
+
+	LineRenderer lr;
+	List<Vector3> ps;
+	float time;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		population = new Cosmos.GaussProdConsResMigPopulation (initialValue, a, pMean, pDev, cMean, cDev, mA);
+		value = population.value;
+		workers = population.workers;
+		resources = population.resources;
+
+		time = 0;
+		ps = new List<Vector3> ();
+		ps.Add (new Vector3 (time, population.value));
+		lr = GetComponent<LineRenderer> ();
+
+		Color color = new Color ((float)Cosmos.Global.random.NextDouble(), (float)Cosmos.Global.random.NextDouble(), (float)Cosmos.Global.random.NextDouble());
+		lr.startColor = color;
+		lr.endColor = color;
+		lr.positionCount = ps.Count;
+		lr.SetPositions (ps.ToArray ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		population.a = a;
+		population.pMean = pMean;
+		population.pDev = pDev;
+		population.cMean = cMean;
+		population.cDev = cDev;
+		population.mA = mA;
+		value = population.value;
+		cRate = population.cRate;
+		nEmigrants = population.nEmigrants;
+	}
+
+	public void UpdatePopulation (float dt)
+	{
+		population.Update (dt);
+	}
+
+	public void Migrate (float dt)
+	{
+		population.Migrate (dt);
+	}
+
+	public void UpdatePlot (float dt)
+	{
+		time += dt;
+		ps.Add (new Vector3 (time, population.value));
+		lr.positionCount = ps.Count;
+		lr.SetPositions (ps.ToArray ());
+	}
+
+	public void AddResource (Cosmos.Resource r)
+	{
+		population.AddResource (r);
+	}
+
+	public void AddPopulation (int idx, Cosmos.GaussProdConsResMigPopulation p)
+	{
+		population.AddPopulation (idx, p);
+	}
+
+	public void RemovePopulation (int idx)
+	{
+		population.RemovePopulation (idx);
+	}
+
+	public void SetWorkers (int idx, float n)
+	{
+		population.SetWorkers (idx, n);
+	}
+
+	public bool CheckExtinction ()
+	{
+		return population.CheckExtinction ();
+	}
+}
